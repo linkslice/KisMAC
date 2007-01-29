a814a2758dd96f4862cf92b0d4486ba3d77766a1
gkruse <gkruse@ab0f578b-8a4a-0410-bf01-fbd21c03f65b>
2007-01-29 21:31:33 +0000

    Forgot two important files on the last commit ;)

---
+++ Sources/WaveDrivers/WaveDriverKismet.h
@@ -0,0 +1,46 @@
+/*
+        
+        File:			WaveDriverKismet.m
+        Program:		KisMAC
+		Author:			Geordie Millar
+						themacuser@gmail.com
+		Description:	Scan with a Kismet server in KisMac.
+                
+        This file is part of KisMAC.
+
+    KisMAC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    KisMAC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with KisMAC; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#import <Foundation/Foundation.h>
+#import "WaveDriver.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+@interface WaveDriverKismet : WaveDriver {
+	int fd;
+	int sockd;
+    struct sockaddr_in serv_name;
+    int status;
+    struct hostent *hp;
+    UInt32 ip;
+	int port;
+	const char *hostname;
+}
+
++ (int) kismetInstanceCount;
+@end
