85f893fd829fa382e3b9e29290cc97e72dd5a69b
gkruse <gkruse@ab0f578b-8a4a-0410-bf01-fbd21c03f65b>
2009-11-09 05:11:11 +0000

    Clarify GPL version

--- Sources/Crypto/AirCrackWrapper.h
+++ Sources/Crypto/AirCrackWrapper.h
@@ -11,9 +11,8 @@
     Most parts of this file are based on aircrack by Christophe Devine.
 
     KisMAC is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    it under the terms of the GNU General Public License, version 2,
+    as published by the Free Software Foundation;
 
     KisMAC is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
